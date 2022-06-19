92cd7a1d5e96a6398c9a9c160f7571f152c6b00b
monm <mounimnotakatsuki@gmail.com>
2022-06-19 22:40:10 +0100

    finished api post + added modal

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,36 +1,81 @@
 import React, { useState } from "react";
+
 import SortableList, { SortableItem } from "react-easy-sort";
 import arrayMove from "array-move";
 
-import styles from "../styles/Main.module.scss";
+import axios from "axios";
+
 import FileCard from "./FileCard";
+import Modal from "./Modal";
+
+import styles from "../styles/Main.module.scss";
 
 function Main() {
   const [files, setFiles] = useState<File[]>([]);
+  const [downloadUrl, setDownUrl] = useState("");
+  const [modal, setModal] = useState(false);
 
+  // display uploaded files
   const handleChange = (e: React.FormEvent) => {
     const { files: pdfFiles } = e.target as HTMLInputElement;
 
     pdfFiles?.length && setFiles((prev) => [...prev, pdfFiles[0]]);
   };
 
+  // hadnle files order sorting
   const onSortEnd = (oldIndex: number, newIndex: number) => {
     setFiles((array) => arrayMove(array, oldIndex, newIndex));
   };
 
+  // post request to merge
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const formData: any = new FormData(e.currentTarget as HTMLFormElement);
+    formData.append("files", files);
+
+    try {
+      const res = await axios.post(
+        process.env.NEXT_PUBLIC_SERVER + "/api/merge",
+        formData,
+        {
+          responseType: "blob",
+        }
+      );
+
+      // create download link
+      const url = window.URL.createObjectURL(new Blob([res.data]));
+      setDownUrl(url);
+    } catch (error) {
+      alert("Something's wrong, Please try again.");
+    }
+  };
+
+  // display modal
+  const displayModal = () => {
+    setModal((prev) => true);
+  };
+
+  // close modal
+  const closeModal = () => {
+    setModal((prev) => false);
+  };
+
+  // < -------- * -------- >
   return (
     <main className={styles.container}>
       <div className={styles.main}>
         <h1>Merge Your Favorite PDF Files</h1>
         <p>Combine Them All With One Click.</p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="file">Select PDF Files</label>
           <input
             onChange={handleChange}
             onClick={(e) => {
               e.currentTarget.value = "";
             }}
+            name="files[]"
             id="file"
             type="file"
             multiple
@@ -41,7 +86,7 @@ function Main() {
           {/* show uploaded files */}
           {files.length > 0 && (
             <div className={styles.filesContainer}>
-              <h3>the order from left to right</h3>
+              <h3>drag the files to change the order</h3>
 
               {/* files uploaded */}
               <SortableList
@@ -61,9 +106,14 @@ function Main() {
 
           {/* convert button */}
           {files.length > 1 && (
-            <button className={styles.button}>Convert</button>
+            <button onClick={displayModal} className={styles.button}>
+              Convert
+            </button>
           )}
         </form>
+
+        {/* modal */}
+        {modal && <Modal url={downloadUrl} closeModal={closeModal} />}
       </div>
     </main>
   );
---
+++ components/Modal.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import React from "react";
+
+import closeIcon from "../public/close.svg";
+import spinner from "../public/spnner.gif";
+
+import styles from "../styles/Modal.module.scss";
+
+type IModal = {
+  url?: string;
+  closeModal: () => void;
+};
+
+function Modal({ url = "", closeModal }: IModal) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        {!url ? (
+          <div className={styles.loading}>
+            <Image src={spinner} alt="" width={240} height={240} />
+            <p>Please wait while we process the files...</p>
+          </div>
+        ) : (
+          <div>
+            <div onClick={closeModal} className={styles.closeIcon}>
+              <Image src={closeIcon} alt="close" layout="fill" />
+            </div>
+
+            <h1>Your PDF File Is Ready!</h1>
+            <a href={url as string} download="merged.pdf">
+              Give It To Me
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
